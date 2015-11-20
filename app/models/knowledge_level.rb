7617b50b5f075fd5a96aaa0a7b616aa90aad9623
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-11-20 19:20:56 -0200

    Add uniqueness validation for knowledge level.

--- app/models/knowledge_level.rb
+++ app/models/knowledge_level.rb
@@ -1,5 +1,6 @@
 class KnowledgeLevel < ActiveRecord::Base
   validates :level, presence: true
+  validates :teacher_id, uniqueness: { scope: :knowledge_group_id }
 
   belongs_to :teacher
   belongs_to :knowledge_group
--- spec/models/knowledge_level_spec.rb
+++ spec/models/knowledge_level_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe KnowledgeLevel, type: :model do
   it { is_expected.to belong_to(:knowledge_group) }
   it { is_expected.to validate_presence_of(:level) }
 
+  it "should validate uniqueness of teacher scoped to knowledge group" do
+    create(:teacher)
+    knowledge_group = create(:knowledge_group)
+    knowledge_level = knowledge_group.knowledge_levels.first
+    expect(knowledge_level).to validate_uniqueness_of(:teacher_id).scoped_to(:knowledge_group_id)
+  end
+
   it "level default value 1" do
     expect(KnowledgeLevel.create.level).to eq(1)
   end
