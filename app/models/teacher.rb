37984e25a9b393df28306914d3252c78ff859a03
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-11-22 15:10:18 -0200

    Correct has_many through association on teacher model.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,7 +4,7 @@ class Teacher < ActiveRecord::Base
   belongs_to :user
   has_many :klasses
   has_many :klass_intents
-  has_many :intents, through: :klass_intents, foreign_key: "klass_id", class_name: "KlassIntent"
+  has_many :intents, through: :klass_intents, source: :teacher
   has_many :knowledge_levels
   has_many :knowledge_groups, through: :knowledge_levels
 
